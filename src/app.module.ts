6f7158750d24ed6d98dbcae5cdf0a2adf909bdec
Ulysses1013 <kanaina99102@gmail.com>
2024-04-28 18:07:02 +0900

    key env setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -42,11 +42,13 @@ const cookieSession = require('cookie-session');
   ],
 })
 export class AppModule {
+  constructor(private configService: ConfigService) {}
+
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(
         cookieSession({
-          keys: ['ascolinpichu'],
+          keys: [this.configService.get('COOKIE_KEY')],
         }),
       )
       .forRoutes('*'); // このMiddlewareをアプリ全体,全てのリクエストで利用;
